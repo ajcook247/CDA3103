671b1b0b5756c0a42870116a5307f7e4b0ebaee9
ajcook247 <43277610+ajcook247@users.noreply.github.com>
2020-04-08 17:05:38 -0400

    Finished ALU(), need to look at handling ALUcontrol

--- project.c
+++ project.c
@@ -7,41 +7,49 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
 	switch (ALUControl)
 	{
-		case '000':
+		// ADD
+		case '0':							// NOTE: Can't treat '000' as a single character
 			*ALUresult = A + B;
 			break;
-			
-		case '001':
+
+		// SUBTRACT
+		case '1':
 			*ALUresult = A - B;
 			break;
 
-		case '010':
+		// SLT (SIGNED)
+		case '2':
 			if (A < B)
 				ALUresult = 1;
 			else
 				ALUresult = 0;
 			break;
 
-		case '011':
+		// SLT (UNSIGNED)
+		case '3':
 			if (A < B)
 				ALUresult = 1;
 			else
 				ALUresult = 0;
 			break;
 
-		case '100':
+		// AND
+		case '4':
 			*ALUresult = A & B;
 			break;
 
-		case '101':
+		// OR
+		case '5':
 			*ALUresult = A | B;
 			break;
 
-		case '110':
+		// SLL B BY 16 BITS
+		case '6':
 			*ALUresult = B << 16;
 			break;
 
-		case '111':
+		// NOT A
+		case '7':
 			*ALUresult = ~A;
 			break;
 
@@ -58,6 +66,11 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 
 /* instruction fetch */
 /* 10 Points */
+
+/*
+1. Fetch the instruction addressed by PC from Mem and write it to instruction.
+2. Return 1 if a halt condition occurs; otherwise, return 0.
+*/
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
 
@@ -66,6 +79,11 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 
 /* instruction partition */
 /* 10 Points */
+
+/*
+1. Partition instruction into several parts (op, r1, r2, r3, funct, offset, jsec).
+2. Read line 41 to 47 of spimcore.c for more information.
+*/
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
 
@@ -75,6 +93,18 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 
 /* instruction decode */
 /* 15 Points */
+
+/*
+1. Decode the instruction using the opcode (op).
+2. Assign the values of the control signals to the variables in the structure controls
+(See spimcore.h file).
+		The meanings of the values of the control signals:
+		For MemRead, MemWrite or RegWrite, the value 1 means that enabled, 0 means
+		that disabled, 2 means “don’t care”.
+		For RegDst, Jump, Branch, MemtoReg or ALUSrc, the value 0 or 1 indicates the
+		selected path of the multiplexer; 2 means “don’t care”.
+		The following table shows the meaning of the values of ALUOp
+*/
 int instruction_decode(unsigned op,struct_controls *controls)
 {
 
@@ -91,6 +121,8 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 
 /* Sign Extend */
 /* 10 Points */
+
+// Assign the sign-extended value of offset to extended_value.
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
 
@@ -98,6 +130,14 @@ void sign_extend(unsigned offset,unsigned *extended_value)
 
 /* ALU operations */
 /* 10 Points */
+
+/*
+1. Apply ALU operations on data1, and data2 or extended_value (determined by ALUSrc).
+2. The operation performed is based on ALUOp and funct.
+3. Apply the function ALU(…).
+4. Output the result to ALUresult.
+5. Return 1 if a halt condition occurs; otherwise, return 0.
+*/
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
 
@@ -105,6 +145,14 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 
 /* Read / Write Memory */
 /* 10 Points */
+
+/*
+1. Base on the value of MemWrite or MemRead to determine memory write
+operation or memory read operation.
+2. Read the content of the memory location addressed by ALUresult to memdata.
+3. Write the value of data2 to the memory location addressed by ALUresult.
+4. Return 1 if a halt condition occurs; otherwise, return 0.
+*/
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
 
@@ -113,6 +161,8 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 
 /* Write Register */
 /* 10 Points */
+
+// 1. Write the data (ALUresult or memdata) to a register (Reg) addressed by r2 or r3.
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
 
@@ -120,6 +170,8 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
 
 /* PC update */
 /* 10 Points */
+
+// 1. Update the program counter (PC).
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
 
