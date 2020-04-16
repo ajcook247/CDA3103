4e28b10acce1a513551af80951c9ab45111612f8
Adam Cook <43277610+ajcook247@users.noreply.github.com>
2020-04-15 20:41:58 -0400

    WIP

--- project.c
+++ project.c
@@ -1,5 +1,13 @@
 #include "spimcore.h"
 
+/* FAILING:
+		afaa0000	# sw $10, 0($29)        	Mem[sp+0] = r10	
+		016a602a	# slt $12, $11, $10    		if(r11<r10) then r12 = 1	$t4 = 0		
+*/
+
+// Joshua Colston
+// Noy Mizrachi
+// Adam Cook
 
 /* ALU */
 /* 10 Points */
@@ -8,7 +16,7 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 	switch (ALUControl)
 	{
 		// ADD
-		case '0':							// NOTE: Can't treat '000' as a single character
+		case '0':
 			*ALUresult = A + B;
 			break;
 
@@ -20,17 +28,17 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 		// SLT (SIGNED)
 		case '2':
 			if (A < B)
-				ALUresult = 1;
+				*ALUresult = 1;
 			else
-				ALUresult = 0;
+				*ALUresult = 0;
 			break;
 
 		// SLT (UNSIGNED)
 		case '3':
 			if (A < B)
-				ALUresult = 1;
+				*ALUresult = 1;
 			else
-				ALUresult = 0;
+				*ALUresult = 0;
 			break;
 
 		// AND
@@ -73,8 +81,11 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-	if (PC % 4 != 0)
+	if ((PC % 4) != 0)
 		return 1; // Word not aligned
+
+	if (PC < 0x0000 || PC > 0xffff)
+		return 1; // out of bounds
 
 	*instruction = Mem[PC >> 2];
 	return 0;
@@ -101,12 +112,13 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 	*/
 
 	// BitMaskOP represents first 6 bits, & with *op to keep
+	/*
 	int opMask 			= 0b11111100000000000000000000000000;
 	int r1Mask 			= 0b00000011111000000000000000000000;
 	int r2Mask 			= 0b00000000000111110000000000000000;
 	int r3Mask 			= 0b00000000000000001111100000000000;
-	int functMask 	= 0b11111100000000000000000000011111;
-	int offsetMask 	= 0b11111100000000001111111111111111;
+	int functMask 		= 0b11111100000000000000000000011111;
+	int offsetMask 		= 0b11111100000000001111111111111111;
 	int jsecMask 		= 0b00000011111111111111111111111111;
 
 	*op = instruction & opMask;
@@ -116,6 +128,15 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 	*funct = instruction & functMask;
 	*offset = instruction & offsetMask;
 	*jsec = instruction & jsecMask;
+	*/
+	
+	*op = (instruction & 0xfc000000) >> 26;
+	*r1 = (instruction & 0x03e00000) >> 21;
+	*r2 = (instruction & 0x001f0000) >> 16;
+	*r3 = (instruction & 0x0000f800) >> 11;
+	*funct = instruction & 0x0000003f;
+	*offset = instruction & 0x0000ffff;
+	*jsec = instruction & 0x03ffffff;
 }
 
 
@@ -151,116 +172,139 @@ int instruction_decode(unsigned op,struct_controls *controls)
 	switch (op)
 	{
 
-		// CANT SWAP BETWEEN CHARACTERS -- NEED THE OPCODE
-		case '0':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// 0x0 - r-type
+		// 0x8 - addi
+		// 0x23 - lw
+		// 0x2b - sw
+		// 0x0f - lui
+		// 0x4 - branch
+		// 0xa - slti
+		// oxb - sltiu
+		// 0x2 - jump
+
+		// r-type
+		case 0:
+			controls->RegDst = '1';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '7';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '0';
+			controls->RegWrite = '1';
 			break;
 
-
-		case '1':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// addi
+		case 8:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '0';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '1';
+			controls->RegWrite = '1';
 			break;
 
-
-		case '2':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// lw
+		case 35:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '1';
+			controls->MemtoReg = '1';
+			controls->ALUOp = '0';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '1';
+			controls->RegWrite = '1';
 			break;
 
-
-		case '3':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// sw
+		case 43:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '0';
+			controls->MemWrite = '1';
+			controls->ALUSrc = '1';
+			controls->RegWrite = '2';
 			break;
 
-
-		case '4':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// lui
+		case 15:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '6';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '1';
+			controls->RegWrite = '1';
 			break;
 
-
-		case '5':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// branch
+		case 4:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '1';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '1';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '0';
+			controls->RegWrite = '1';
 			break;
 
-
-		case '6':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// slti
+		case 10:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '2';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '1';
+			controls->RegWrite = '1';
 			break;
 
+		// sltiu
+		case 11:
+			controls->RegDst = '0';
+			controls->Jump = '0';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '3';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '1';
+			controls->RegWrite = '1';
+			break;
 
-		case '7':
-			Controls->RegDst = '';
-			Controls->Jump = '';
-			Controls->Branch = '';
-			Controls->MemRead = '';
-			Controls->MemtoReg = '';
-			Controls->ALUOp = '';
-			Controls->MemWrite = '';
-			Controls->ALUSrc = '';
-			Controls->RegWrite = '';
+		// jump
+		case 2:
+			controls->RegDst = '0';
+			controls->Jump = '1';
+			controls->Branch = '0';
+			controls->MemRead = '0';
+			controls->MemtoReg = '0';
+			controls->ALUOp = '2';
+			controls->MemWrite = '0';
+			controls->ALUSrc = '2';
+			controls->RegWrite = '2';
 			break;
 
 		default:
 			return 1;
-			break;
 
-		return 0;
 	}
+	
+	return 0;
 }
 
 /* Read Register */
@@ -278,7 +322,14 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 // Assign the sign-extended value of offset to extended_value.
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
+	// negative = fill 1, positive = fill 0
+	unsigned extends = 0xffff0000;
+	unsigned negative = offset >> 16;
 
+	if (negative == 1)
+		*extended_value = offset | extends;
+	else
+		*extended_value = offset & 0x0000ffff;
 }
 
 /* ALU operations */
@@ -293,7 +344,120 @@ void sign_extend(unsigned offset,unsigned *extended_value)
 */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
+	if (ALUOp == '7')
+	{
+		switch (funct)
+		{
+			// add
+			case 32:
+				ALUOp = '0';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+			
+			// subtract
+			case 34:
+				ALUOp = '1';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+				
+			// slt
+			case 42:
+				ALUOp = '2';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+			
+			// slti
+			case 43:
+				ALUOp = '3';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+			
+			// and
+			case 36:
+				ALUOp = '4';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+			
+			// or			
+			case 37:
+				ALUOp = '5';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+				
+			// sll
+			case 6:
+				ALUOp = '6';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+				
+			// not
+			case 39:
+				ALUOp = '7';
+				ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+				
+			default:
+				return 1;
+		}
+	}
+	else
+	{
+		switch (ALUOp)
+		{
+			case '0': // add
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
 
+			case '1': // subtract
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+
+			case '2': // slt (signed)
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+
+			case '3': // slt (unsigned)
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+
+			case '4': // and
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+
+			case '5': // or
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+
+			case '6': // sll
+				if (ALUSrc == '1')
+					ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+				else
+					ALU(data1, data2, ALUOp, ALUresult, Zero);
+				break;
+
+			default:
+				return 1;
+		}
+	}
+	return 0;
 }
 
 /* Read / Write Memory */
@@ -302,13 +466,30 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 /*
 1. Base on the value of MemWrite or MemRead to determine memory write
 operation or memory read operation.
-2. Read the content of the memory location addressed by ALUresult to memdata.
-3. Write the value of data2 to the memory location addressed by ALUresult.
+2. Read the content of the memory location addressed by ALUresult to memdata. (LW)
+3. Write the value of data2 to the memory location addressed by ALUresult. (SW)
 4. Return 1 if a halt condition occurs; otherwise, return 0.
 */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
+	if (MemWrite == '1' && MemRead == '1')
+		return 1;
+	
+	if ((MemWrite == '1' || MemRead == '1'))
+	{
+		if (ALUresult % 4 != 0)
+			return 1;
+		if (ALUresult < 0x000 || ALUresult > 0xffff)
+			return 1;
+	}
 
+	if (MemWrite == '1' && MemRead == '0') // sw
+		Mem[ALUresult >> 2] = data2;
+
+	if (MemWrite == '0' && MemRead == '1') // lw
+		*memdata = Mem[ALUresult >> 2];
+
+	return 0;
 }
 
 
@@ -318,7 +499,15 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 // 1. Write the data (ALUresult or memdata) to a register (Reg) addressed by r2 or r3.
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+	// R: r3
+	// I: r2
 
+	if (RegWrite == '1' && MemtoReg == '1')
+		Reg[r2] = memdata;
+	else if (MemtoReg == '0' && RegDst == '1')
+		Reg[r3] = ALUresult;
+	else
+		Reg[r2] = ALUresult;
 }
 
 /* PC update */
@@ -327,5 +516,14 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
 // 1. Update the program counter (PC).
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
-
+	if (Branch == '0' && Jump == '0')
+		*PC = *PC + 4;
+	else if (Branch == '1' && Jump == '0')
+		*PC = (extended_value << 2) + (*PC + 4);
+	else if (Branch == '0' && Jump == '1')
+	{
+		unsigned shift = jsec << 2;
+		unsigned bits = (*PC + 4) | 0xF0000000;
+		*PC = bits | shift;
+	}
 }
